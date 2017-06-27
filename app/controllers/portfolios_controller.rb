1929ac4b459d3d72a8613a652dcb4ed6e8767dca
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:58:37 +0000

    create And new Actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+  
+  def new
+    @portfolio = Portfolio.new
+  end
+  
+  def create
+    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolio_path, notice: 'Portfolio Added' }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
